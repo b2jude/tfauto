c6a9f2a07d7e15429b4429455b17bd32bf4113a1
b2jude <b2jude@outlook.com>
2018-09-25 01:11:46 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ COMMAND=$1
 echo 'executing terraform...'
 echo 'executing pass init'
 /usr/local/bin/terraform init
-if [ "${COMMAND}" == "plan"];
+if [ "$COMMAND" = "plan"];
  then
-   /usr/local/bin/terraform ${COMMAND} -input=false
+   /usr/local/bin/terraform $COMMAND -input=false
 else
 /usr/local/bin/terraform ${COMMAND} -auto-approve
 #/usr/local/bin/terraform $apply -auto-approve 
