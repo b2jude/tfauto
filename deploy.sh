513f2a0351744ea5a29d99900904430ce398351a
b2jude <b2jude@outlook.com>
2018-09-25 00:42:34 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo 'executing terraform...'
 echo 'executing pass init'
-/usr/local/bin/terraform init
-/usr/local/bin/terraform plan -input=false
-/usr/local/bin/terraform apply -auto-approve 
-
+#/usr/local/bin/terraform init
+#/usr/local/bin/terraform plan -input=false
+#/usr/local/bin/terraform apply -auto-approve 
+/usr/local/bin/terraform destroy -auto-approve
