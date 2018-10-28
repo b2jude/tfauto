31b457e92d3216439a36c7da8c091f6b47617d70
b2jude <b2jude@outlook.com>
2018-10-28 12:22:34 -0400

    Added policiesAndroles subsystem

---
+++ policiesAndroles/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+COMMAND=$1
+echo "You are deploying ${SUBSYSTEM} \n"
+echo "Your COMMAND is: ${COMMAND}"
+
+export TF_VAR_region=${REGION}
+export TF_VAR_appname=${APP_NAME}
+
+     echo "****** backend configuration options ************"
+
+
+     echo "bucket=tf-developer"
+     echo "key=${APP_NAME}/${SUBSYSTEM}/${REGION}/${ENVIRONMENT}/${RELEASE}.tfstate"
+     echo "profile=developer"
+     echo "${REGION}"
+     terraform init \
+     -backend-config="bucket=tf-developer" \
+     -backend-config="key=${APP_NAME}/${SUBSYSTEM}/${REGION}/${ENVIRONMENT}/${RELEASE}.tfstate" \
+     -backend-config="region=${REGION}" \
+     -backend-config="profile=developer"
+
+
+
+
+#/usr/local/bin/terraform init
+#terraform init
+
+if [ "${COMMAND}" = 'plan' ];
+ then
+   #/usr/local/bin/terraform ${COMMAND} -input=false
+   terraform ${COMMAND} -input=false
+else
+#/usr/local/bin/terraform ${COMMAND} -auto-approve
+terraform ${COMMAND} -auto-approve
+#/usr/local/bin/terraform $apply -auto-approve
+#/usr/local/bin/terraform destroy -auto-approve
+fi;
---
+++ policiesAndroles/roles.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_role" "asg_ec2_role" {
+    name = "${var.appname}_asg_ec2_role"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+  },
+      "Effect": "Allow",
+      "Sid": ""
+      }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy" "asg_policy" {
+    name = "${var.appname}_asg_policy"
+    role = "${aws_iam_role.asg_ec2_role.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "autoscaling:*",
+      "Resource": "*"
+    },
+    {
+        "Effect": "Allow",
+        "Action": "cloudwatch:PutMetricAlarm",
+        "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_instance_profile" "asg_profile" {
+    name = "${var.appname}_asg_profile"
+    role = "${aws_iam_role.asg_ec2_role.name}"
+}
