4e7f339a3fa7566a1f14f2c5dcd1523f119f00c1
b2jude <b2jude@outlook.com>
2018-11-07 22:24:55 -0500

    Added route53 cname record

--- webapp_server/route53-dns.tf
+++ webapp_server/route53-dns.tf
@@ -1,7 +1,7 @@
 
 resource "aws_route53_record" "webapp_simple_cname_record" {
          zone_id = "Z10TFNYAKKKBN"
-         name    = "${lookup(local.stack_labels, "appname")}${lookup(local.stack_labels,"stack_version")}-${lookup(local.stack_labels,"region")}.bmonoue.net"
+         name    = "${lookup(local.webapp_stacklabels, "appname")}${lookup(local.webapp_stacklabels,"stack_version")}-${lookup(local.webapp_stacklabels,"region")}.bmonoue.net"
          type = "${var.type}"
          ttl = "${var.ttl}"
          records =  ["${module.webappasg.alb_dnsname}"]
