a02c56e22729a6c7735d43e24b6097f4f19018f8
b2jude <b2jude@outlook.com>
2018-11-07 21:20:05 -0500

    Added route53 cname record

--- webapp_server/variable.tf
+++ webapp_server/variable.tf
@@ -1,27 +1,11 @@
-#variable "webapp_vpcid" {}
+
 
 variable "webapp_appname" {}
-#variable "webapp_ami_id" {}
 variable "webapp_instancetype" {}
 
-#variable "webapp_instanceprofile" {}
-
-#variable "webapp_userdata" {}
-#variable "webapp_instance_securitygroup" {}
-#variable "webapp_asg_subnets" {}
-#variable "webapp_instanceport" {}
-#variable "webapp_instanceprotocol" {}
-
-#variable "webapp_lbport" {}
-#variable "webapp_lbprotocol" {}
-#variable "webapp_healthythreshold" {}
-#variable "webapp_unhealthythreshold" {}
 variable "webapp_max_size" {}
 variable "webapp_min_size" {}
 variable "webapp_desirecapacity" {}
-#variable "webapp_timeout" {}
-#variable "webapp_interval" {}
-
 
 variable "stack_version" {}
 variable "region" {}
@@ -32,4 +16,12 @@ locals {
       stack_version = "${var.stack_version}"
       region =  "${var.region}"
   }
+}
+
+variable "type" {
+  default = "CNAME"
+}
+
+variable "ttl" {
+  default = 300
 }
