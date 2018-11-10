088073aaf07a2bd161e096e9630efe3aa24385b0
b2jude <b2jude@outlook.com>
2018-11-09 21:39:35 -0500

    added live module

---
+++ webapp-Blue-Green-deployment/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "${var.region}"
+  profile = "developer"
+  alias   = "dev_account"
+}
+
+
+resource "aws_route53_record" "webapp_live_cname_record" {
+         zone_id = "Z10TFNYAKKKBN"
+         name    = "${lookup(local.webapp_stacklabels, "appname")}${lookup(local.webapp_stacklabels,"region")}.bmonoue.net"
+         type = "${var.type}"
+         ttl = "${var.ttl}"
+         records =  ["${var.live_url}"]
+ }
---
+++ webapp-Blue-Green-deployment/variable.tf
@@ -0,0 +1,28 @@
+
+
+
+variable "webapp_instancetype" {}
+variable "webapp_max_size" {}
+variable "webapp_min_size" {}
+variable "webapp_desirecapacity" {}
+variable "stack_version" {}
+
+
+variable "webapp_appname" {}
+variable "live_url" {}
+variable "region" {}
+locals {
+  webapp_stacklabels = {
+     appname = "${var.webapp_appname}"
+      stack_version = "${var.stack_version}"
+      region =  "${var.region}"
+  }
+}
+
+variable "type" {
+  default = "CNAME"
+}
+
+variable "ttl" {
+  default = 300
+}
