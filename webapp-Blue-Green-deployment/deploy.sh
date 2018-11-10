97a0e3b9506c9cde706733de668e3d647bfc4f2d
b2jude <b2jude@outlook.com>
2018-11-09 22:49:49 -0500

    added live module

--- webapp-Blue-Green-deployment/deploy.sh
+++ webapp-Blue-Green-deployment/deploy.sh
@@ -2,20 +2,20 @@
 COMMAND=$1
 
 export TF_VAR_region=${REGION}
-#export TF_VAR_stack_version=${STACK_VERSION}
+export TF_VAR_stack_version=${STACK_VERSION}
 export TF_VAR_webapp_appname=${APP_NAME}
 export TF_VAR_live_url=${LIVE_URL}
 
 
      echo "****** backend configuration options ************"
      echo "bucket=tf-developer"
-     echo "key=${APP_NAME}/${REGION}/${ENVIRONMENT}/live.tfstate"
+     echo "key=${APP_NAME}/${REGION}/${ENVIRONMENT}/${STACK_VERSION}.tfstate"
      echo "profile=developer"
      echo "${REGION}"
      terraform init \
      -backend-config="bucket=tf-developer" \
     # -backend-config="key=${APP_NAME}/${REGION}/${ENVIRONMENT}/${STACK_VERSION}.tfstate" \
-     -backend-config="key=${APP_NAME}/${REGION}/${ENVIRONMENT}/live.tfstate" \
+     -backend-config="key=${APP_NAME}/${REGION}/${ENVIRONMENT}/${STACK_VERSION}.tfstate" \
      -backend-config="region=${REGION}" \
      -backend-config="profile=developer"
 
