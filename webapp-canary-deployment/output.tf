d8db0fc7cadf33f639a311f31bab18846e0ae2d9
b2jude <b2jude@outlook.com>
2018-11-10 21:28:22 -0500

    added canary deployment

--- webapp-canary-deployment/output.tf
+++ webapp-canary-deployment/output.tf
@@ -1,5 +1,9 @@
 
 
-output "webapp_live_record" {
-   value = "${aws_route53_record.webapp_live_cname_record.name}"
+output "canarywebapp_dev_record" {
+   value = "${aws_route53_record.canary_webapp_dev_cname_record.name}"
+}
+
+output "canarywebapp_live_record" {
+   value = "${aws_route53_record.canary_webapp_live_cname_record.name}"
 }
