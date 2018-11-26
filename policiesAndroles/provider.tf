9953900d43695aa75e83ead05958864fe2f41ee1
b2jude <b2jude@outlook.com>
2018-11-25 20:45:17 -0500

    cross account role

--- policiesAndroles/provider.tf
+++ policiesAndroles/provider.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "${var.region}"
   profile = "admin"
-
+  alias = "adminaccount"
 }
 provider "aws" {
   region = "${var.region}"
   profile = "dev"
-
+  alias = "devaccount"
 }
