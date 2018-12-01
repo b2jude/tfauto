8a643b24aa89391ea6cba663c1c2b6044241e11e
b2jude <b2jude@outlook.com>
2018-12-01 18:07:26 -0500

    updated provider and backend encryption

--- webapp_server/provider.tf
+++ webapp_server/provider.tf
@@ -1,12 +1,7 @@
 /*
 provider "aws" {
   region = "${var.region}"
-  profile = "developer"
-  alias   = "dev_account"
-}
-*/
-provider "aws" {
-  region = "${var.region}"
   profile = "devaccount"
   alias = "dev.account"
 }
+*/
